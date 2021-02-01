e9f1e8535674e7812941b542877a87ecd09bb628
gargximran <gargximran@gmail.com>
2021-02-02 01:56:37 +0600

    init app

---
+++ Application/__init__.py
@@ -0,0 +1,34 @@
+# Import Flask Things
+from flask import Flask
+from flask_cors import CORS
+from flask_mongoengine import MongoEngine
+
+# import Helpers
+from Application.helpers import env
+
+# import error handler
+from .errors import handle_404_errors, handle_500_errors
+
+
+# app fire
+def create_app(debug=False):
+    """Create an application"""
+    app = Flask(__name__)
+    app.debug = debug
+
+    # app configuration's
+    app.config['SECRET_KEY'] = env.get('SECRET_KEY')
+    app.config['MONGODB_SETTINGS'] = env.get('MONGODB_SETTINGS')
+
+    MongoEngine(app)
+    CORS(app)
+
+    # error handling
+    app.register_error_handler(404, handle_404_errors)
+    app.register_error_handler(500, handle_500_errors)
+
+    return app
+
+
+
+
